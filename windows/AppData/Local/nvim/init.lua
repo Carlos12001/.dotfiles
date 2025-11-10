9051f359177a0e95113737acadf8ac618847055c
Carlos Andres Mata Calderon <62664256+Carlos12001@users.noreply.github.com>
2025-11-10 17:47:54 -0600

    refactor: set clipboard to unnamedplus and adjust indentation
    Configured Neovim settings for clipboard and indentation.

--- windows/AppData/Local/nvim/init.lua
+++ windows/AppData/Local/nvim/init.lua
@@ -6,3 +6,15 @@ opts = {
     hererocks = false,
   },
 }
+
+-- Use system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Indentation: 2 spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+-- Vertical line at column 80
+vim.opt.colorcolumn = "80"
